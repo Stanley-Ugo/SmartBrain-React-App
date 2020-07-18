b2cfcf4712b80b4aeda4012c452c0b36879bd872
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-18 16:42:14 +0100

    Added onInputChange & onButtonSubmit click events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
 
+
+const app = new Clarifai.App({
+  apiKey : 'b514d7b4548a4865bbfd8dcd8fb166dc',
+});
 
 const particlesOptions = {
   particles: {
@@ -16,20 +21,49 @@ const particlesOptions = {
         value_area : 800
       }
     }
-  },
+  }
 };
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-      params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+
+class App extends Component { 
+  constructor() {
+    super();
+    this.state = {
+      input : ''
+    }
+  }
+
+  oninputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+    app.models.predict("b514d7b4548a4865bbfd8dcd8fb166dc", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response){
+        console.log(response);
+      },
+      function(err){
+        //there was an error
+      }
+    )
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+        params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          oninputChange={ this.oninputChange } 
+          onButtonSubmit={this.onButtonSubmit}/>
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ oninputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className="f3">
@@ -9,8 +9,9 @@ const ImageLinkForm = () => {
       </p>
       <div className="center">
         <div className='form center pa4 br3 shadow-5'>
-          <input className="f4 pa2 w-70 center" type="text" />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">
+          <input className="f4 pa2 w-70 center" type="text" onChange={oninputChange}/>
+          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}>
             Detect
           </button>
         </div>
