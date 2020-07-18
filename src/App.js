65ca4f4bc6d9530f0268aec59b9d811255a56f91
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-07-18 15:11:54 +0100

    Enabled and styled Particles.js in the App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import './App.css';
+import React from "react";
+import Particles from 'react-particles-js';
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
 
+
+const particlesOptions = {
+  particles: {
+    number : {
+      value : 60,
+      density : {
+        enable : true,
+        value_area : 800
+      }
+    }
+  },
+};
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+      params={particlesOptions} />
       <Navigation />
-      <Logo/>
-      <Rank/>
-      <ImageLinkForm/>
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
       {/*<FaceRecognition />*/}
     </div>
   );
